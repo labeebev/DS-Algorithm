c76d9f8cad6be2fda35bda581fe35b9b1941a5ea
Labeeb EV <labeebev10@gmail.com>
2021-09-29 01:29:35 +0530

    Update Leapyear.cpp

--- C++/Leapyear.cpp
+++ C++/Leapyear.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     int yr;
+    cout<<"Enter year to check : ";
     cin>>yr;
     if((yr%4==0 && yr%100!=0) || yr%400==0){
         cout<<yr<<" is a leap year";
