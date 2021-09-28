8155eb483149c0b080d57c8d71f3982ca0e92559
Labeeb EV <labeebev10@gmail.com>
2021-09-29 02:43:16 +0530

    PGM 5
    To calculate Simple Interest

---
+++ Python/SimpleInterest.py
@@ -0,0 +1,8 @@
+print("Enter Principal Amount : ",end='')
+p=float(input())
+print("Rate of Interest in % : ",end='')
+r=float(input())
+print("Time (in years) : ",end='')
+t=float(input())
+s=p*r*t/100;
+print("Simple Interest :",s);
