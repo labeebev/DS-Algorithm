89200b3e0f1b441f45917eb11bffeefd8d203819
Labeeb EV <labeebev10@gmail.com>
2021-09-30 00:04:57 +0530

    PGM 6
    LCM of two numbers

--- Java/LCM.java
+++ Java/LCM.java
@@ -2,7 +2,7 @@ package com.lev;
 
 import java.util.Scanner;
 
-public class Main {
+public class LCM {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the numbers : ");
