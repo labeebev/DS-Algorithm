58008696cbb8d5f2bfbbbc29ea5b0d430135bb5e
Labeeb EV <labeebev10@gmail.com>
2021-10-01 18:09:36 +0530

    PGM 9
    Area Of Circle C++ Program

---
+++ C++/Area_Circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+const float PI=3.14;
+int main(){
+    int r;
+    cout<<"Enter the radius : ";
+    cin>>r;
+    cout<<"Area : "<<(PI*r*r);
+    return 0;
+}
