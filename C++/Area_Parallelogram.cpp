410dff94a88f57062fb3be78a5e2c614c4c9e123
Labeeb EV <labeebev10@gmail.com>
2021-10-08 22:39:53 +0530

    PGM 13
    Area of parallelogram

---
+++ C++/Area_Parallelogram.cpp
@@ -0,0 +1,12 @@
+//AREA OF PARALLELOGRAM
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,area;
+    cout<<"Enter base, height : ";
+    cin>>b>>h;
+    area = b*h;
+    cout<<"Area : "<<area;
+    return 0;
+}
